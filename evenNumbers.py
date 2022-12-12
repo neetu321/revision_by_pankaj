5314c0a6ca789c353f58f9f2a73602f4e57c17ef
neetu321 <cdc.psn@gmail.com>
2022-12-12 13:04:26 +0530

    first commit on dec12

---
+++ evenNumbers.py
@@ -0,0 +1,6 @@
+num=int(input("enter the number till where you need even numbers"))
+for i in range(num+1):
+    if i%2==0:
+        print(i)
+
+
---
+++ evenNumbersWhileLoop.py
@@ -0,0 +1,6 @@
+i=0
+num=int(input("enter the number till where you need even numbers"))
+while i<=num:
+    if i%2==0:
+        print(i)
+    i=i+1
